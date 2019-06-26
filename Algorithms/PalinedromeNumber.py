ff2a5727f854f9e138cbf41318db3b2fc8ae0384
Stewart Young <stewart@dresser.app>
2019-06-26 09:52:15 +0100

    correct palindrome checker

--- Algorithms/PalinedromeNumber.py
+++ Algorithms/PalinedromeNumber.py
@@ -1,10 +1,11 @@
 example1 = 121  # Should return true
 example2 = -121  # Should return false
+example3 = 1000021  # Should return true
 
 
 def PalinedromeNumber(x: int) -> bool:
     string = str(x)
-    if string[0] == string[-1]:
+    if string == string[::-1]:
         return True
     else:
         return False
@@ -12,3 +13,4 @@ def PalinedromeNumber(x: int) -> bool:
 
 print(PalinedromeNumber(example1))
 print(PalinedromeNumber(example2))
+print(PalinedromeNumber(example3))
