94b74c8407dfe3bee2459ee7873d8589fc25b059
Stewart Young <stewart@dresser.app>
2019-06-30 11:34:29 +0100

    Made working version that doesn't use ListNode's

---
+++ Algorithms/MergeTwoSortedLists.py
@@ -0,0 +1,22 @@
+egList1 = [1,2,4]
+egList2 = [1,3,4]
+
+def mergeTwoSortedLists(test_list1, test_list2):
+	size_1 = len(test_list1) 
+	size_2 = len(test_list2) 
+	res = [] 
+	i, j = 0, 0
+	while i < size_1 and j < size_2: 
+		if test_list1[i] < test_list2[j]: 
+			res.append(test_list1[i]) 
+			i += 1
+
+		else: 
+			res.append(test_list2[j]) 
+			j += 1
+
+	res = res + test_list1[i:] + test_list2[j:]
+
+	return res
+
+print(mergeTwoSortedLists(egList1, egList2))
