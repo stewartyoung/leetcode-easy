e4ba87a869b33f68338dea10ba3a0efc8b98cf90
Stewart Young <stewart@dresser.app>
2019-07-02 10:39:11 +0100

    Another v, works in terminal but not on leetcode

--- Algorithms/RemoveDuplicatesFromSortedArray.py
+++ Algorithms/RemoveDuplicatesFromSortedArray.py
@@ -3,9 +3,9 @@ egNums2 = [0,0,1,1,1,2,2,3,3,4]
 
 def removeDuplicates(nums) -> int:
 	uniqueSet = []
-	for number in nums:
-		if number not in uniqueSet:
-			uniqueSet.append(number)
+	for number in range(len(nums)):
+		if nums[number] not in uniqueSet:
+			uniqueSet.append(nums[number])
 	return uniqueSet
 
 print(removeDuplicates(egNums1))
