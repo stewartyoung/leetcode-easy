023667297a0ff8b5d585fe2c11e18b772355b740
Stewart Young <stewart@dresser.app>
2019-07-10 14:21:37 +0100

    first attempt at remove element

--- Algorithms/RemoveElement.py
+++ Algorithms/RemoveElement.py
@@ -1,1 +1,11 @@
+egnums = [3,2,2,3]
+egval = 3
 
+def RemoveElement(nums, val):
+	for i = 0: #range(len(nums)):
+		if val == nums[i]:
+			nums.pop(i)
+			i = i - 1
+	return len(nums)
+
+RemoveElement(egnums, egval)
