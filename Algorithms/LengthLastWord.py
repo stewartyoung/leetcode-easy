9fd4bc00ef60a5c3c96bfc9dd73ad3267c625855
Stewart Young <stewartyoung97@gmail.com>
2019-07-30 09:45:20 +0100

    Add LengthLastWord

---
+++ Algorithms/LengthLastWord.py
@@ -0,0 +1,20 @@
+input = "Hello World"
+
+def LengthLastWord(string):
+    """
+    Time:  O(n)
+    Space: O(1)
+    """
+    # Skip any trailing spaces
+    i = len(string) - 1
+    while i >= 0 and string[i] == ' ':
+        i -= 1
+
+    # Iterate through the last word
+    j = i
+    while j >= 0 and string[j] != ' ':
+        j -= 1
+
+    return i - j
+
+print(LengthLastWord(input))
