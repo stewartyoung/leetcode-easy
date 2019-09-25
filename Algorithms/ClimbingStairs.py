984d40795a4858ba7083a8bbd8898d2ca1bc48bc
Stewart Young <stewart@Stewarts-MacBook-Pro.local>
2019-09-25 07:32:39 +0100

    Fixed unused variable problem

--- Algorithms/ClimbingStairs.py
+++ Algorithms/ClimbingStairs.py
@@ -6,7 +6,7 @@ def ClimbingStairs(n):
         return n
     prev = 1
     prev2 = 2
-    for numSteps in range(3,n+1):
+    for _ in range(3,n+1):
         current = prev + prev2
         prev = prev2
         prev2 = current
