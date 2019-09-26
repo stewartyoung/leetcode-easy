2c8209bf10434ca1c90ad5fa8301483339d33f55
Stewart Young <stewart@Stewarts-MacBook-Pro.local>
2019-09-26 07:51:10 +0100

    Fixed typos

--- Algorithms/LinkedList.py
+++ Algorithms/LinkedList.py
@@ -18,20 +18,21 @@ class LinkedList(object):
         return self.size
     def add (self,d):
         new_node = Node (d,self.root)
-        self.root = new_nodeself.size += 1
+        self.root = new_node
+        self.size += 1
     def remove(self, d):
         this_node = self.root
         prev_node = None
         while this_node:
             if this_node.get_data() == d:
                 if prev_node:
-                    prev_node.set_next(this.node.get_next())
+                    prev_node.set_next(this_node.get_next())
                 else:
                     self.root = this_node
                 self.size -= 1
                 return True
             else:
-                prev_node = this.node
+                prev_node = this_node
                 this_node = this_node.get_next()
         return False
     def find(self,d):
